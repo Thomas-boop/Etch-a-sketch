1ea6271456926e864636dc48295ac747d6905db2
Thomas_Thabethe <thomasthabethe3@gmail.com>
2023-01-24 15:27:10 +0200

    done

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-const container = document.getElementById("container");
-
-        const gridContainer = document.createElement("div");
-        gridContainer.classList.add("grid-container");
-
-        for (let i = 0; i < 16 * 15; i++) {
-            const gridSquare = document.createElement("div");
-            gridSquare.classList.add("grid-square");
-            gridContainer.appendChild(gridSquare);
-        }
-
-        container.appendChild(gridContainer);
 
 
-let changeSizeButton = document.getElementById("body-btn");
-changeSizeButton.addEventListener("click", function () {
-
-    let size = prompt("Please enter new grid size e.g(18)");
-
+function gridprinter(gridsize){
     
-  })
+    const container = document.getElementById("container");
+    
+            const gridContainer = document.createElement("div");
+            gridContainer.classList.add("grid-container");
+    
+            for (let i = 0; i < gridsize * gridsize; i++) {
+                const gridSquare = document.createElement("div");
+                gridSquare.classList.add("grid-square");
+                gridContainer.appendChild(gridSquare);
+            }
+    
+            container.appendChild(gridContainer);
+    
+}
 
+
+    let changeSizeButton = document.getElementById("body-btn");
+    changeSizeButton.addEventListener("click", function () {
+    
+        let size = prompt("Please enter new grid size e.g(18)");
+    
+        gridprinter(size)
+    
+      })
